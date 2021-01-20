ce030a9f3b10a22fe7456479bed500f447f77074
zengd <309606298@qq.com>
2021-01-20 10:59:22 +0800

    .

--- spring-boot-learn/src/main/java/com/zengdw/learn/event/UserEventListener.java
+++ spring-boot-learn/src/main/java/com/zengdw/learn/event/UserEventListener.java
@@ -2,9 +2,12 @@ package com.zengdw.learn.event;
 
 import com.zengdw.learn.domain.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 /**
  * @author : zengd
@@ -15,9 +18,9 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class UserEventListener {
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     public void handleEvent(User user){
-        log.info(user.toString());
+        log.info("Transactional: {}", user.toString());
     }
 
     @Async
@@ -26,5 +29,13 @@ public class UserEventListener {
         log.info(Thread.currentThread().getName());
         log.info(obj);
         throw new Exception("exception");
+    }
+
+    /**
+     * 监听上下文刷新事件
+     */
+    @EventListener
+    public void handleEvent2(ContextRefreshedEvent cse) {
+
     }
 }
