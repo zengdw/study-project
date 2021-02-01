f0cb821b8a1ed0c6459612a81d6820dfa26163dd
zengd <309606298@qq.com>
2021-02-01 10:14:09 +0800

    .

--- spring-boot-learn/src/test/java/com/zengdw/learn/LearnApplicationTests.java
+++ spring-boot-learn/src/test/java/com/zengdw/learn/LearnApplicationTests.java
@@ -1,20 +1,13 @@
 package com.zengdw.learn;
 
-import com.zengdw.learn.event.UserEventPublisher;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.annotation.Resource;
 
 @SpringBootTest
 class LearnApplicationTests {
 
-    @Resource
-    UserEventPublisher register;
-
     @Test
     void contextLoads() {
-        register.register();
     }
 
 }
